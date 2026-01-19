c141e4a1b891ca80e60d42b27bd121d4ebf250b3
sa7326wo <saskia.wohlers@fh-erfurt.de>
2026-01-19 10:16:02 +0100

    changes in folder structure; separated frontend/backend; new dockerfile for php; etc

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,13 +1,10 @@
-# Nutze ein PHP-Apache Image fuer statische Dateien und API
 FROM php:8.2-apache
 
-# PostgreSQL PDO-Treiber installieren
 RUN apt-get update \
     && apt-get install -y --no-install-recommends libpq-dev \
     && docker-php-ext-install pdo pdo_pgsql \
     && rm -rf /var/lib/apt/lists/*
 
-# Projektdateien ins Webroot kopieren
-COPY . /var/www/html
+COPY ./api /var/www/html
 
 EXPOSE 80
